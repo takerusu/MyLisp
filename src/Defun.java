b2ca85816a983ce25473760838f58262b83a68a1
takerusu <takeru.sudo.at@gmail.com>
2014-04-14 18:10:21 +0900

    add defun

---
+++ src/Defun.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Defun {
+	public Defun() {
+		
+	}
+	public ArrayList<String> name = new ArrayList<String>();
+	public ArrayList<ArrayList<String>> symbol = new ArrayList<ArrayList<String>>();
+	public ArrayList<ArrayList<String>> value = new ArrayList<ArrayList<String>>();
+	public STree cell = new STree();
+	
+}
